a8a898cbff9340217932f6f2974a2e2984b4c1ba
Maria Kulikova <m.kulikova@itransition.com>
2018-10-16 12:07:34 +0300

    added template

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import {render} from 'react-dom';
+import Article from './Article';
+
+export function App() {
+  return (
+    <div>
+      <h1>App name +++</h1>
+      <Article/>
+    </div>
+  );
+}
